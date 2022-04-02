85c09319d5090ffbed72505a32d6d21b6d176d9b
Artyom Holubov <artyom.holubov@neptuness.com>
2022-04-02 23:28:20 +0300

    Header update

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,11 +5,9 @@ import Game from "../store/game";
 import logo from "../logo.svg";
 
 const Header = observer(({type}) => {
-    const {x, y} = FigureStore;
-
     return (
         <>
-            <header className="App-header">
+            <header className="app-header">
                 <img src={logo} className="App-logo" alt="logo"/>
                 <p>
                     <code>Tetris</code>
@@ -18,13 +16,9 @@ const Header = observer(({type}) => {
                 </p>
             </header>
             <style jsx>{`
-              .figure-wrp {
-                position: absolute;
-                left: ${x}px;
-                top: ${y}px;
-                height: 50px;
-                width: 50px;
-                background-color: red;
+              .app-header {
+                display: flex;
+                align-items: center;
               }
             `}</style>
         </>
